c32697fb07656983fa09fa558fbc5ad12a174622
Mikhail Paliy <mikhail.paliy@rt.ru>
2023-02-28 00:46:18 +0300

    Defined base DB models + added tengwar font

---
+++ app/models/exercise.py
@@ -0,0 +1,8 @@
+from app import db
+
+class Exercise(db.Model):
+    __tablename__ = 'exercises'
+    id = db.Column(db.Integer, primary_key=True)
+    question = db.Column(db.String(200), nullable=False)
+    answer = db.Column(db.String(200), nullable=False)
+    hint = db.Column(db.String(100))
