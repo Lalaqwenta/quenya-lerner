7edebb6d57aff24a32cc8217ee5c65f26f2f7c8e
Mikhail Paliy <mikhail.paliy@rt.ru>
2023-02-28 21:33:22 +0300

    Added login route

--- qlerner/models/user.py
+++ qlerner/models/user.py
@@ -1,4 +1,5 @@
 from qlerner.main.database import db
+from qlerner.main.login_manager import lm
 
 class User(db.Model):
     __tablename__ = "users"
@@ -7,3 +8,11 @@ class User(db.Model):
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(60), nullable=False)
     exercises = db.relationship('Exercise', backref='user', lazy=True)
+    is_active = False
+
+    def check_password(self, _password: str) -> bool:
+        return _password == self.password
+
+@lm.user_loader
+def user_loader(user):
+    return User.get(user)
