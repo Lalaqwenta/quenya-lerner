0e019876cea2f175001cb8acac9d9dc2381586d9
Lalaqwenta <plmisha97@gmail.com>
2023-03-02 00:58:21 +0300

    Added solvable exercises

--- qlerner/models/exercise.py
+++ qlerner/models/exercise.py
@@ -11,8 +11,16 @@ class Exercise(db.Model):
     hint = db.Column(db.String(100))
     tags = db.Column(db.String)  # comma-separated list of tags
 
-completed_exercises_NAMETAG = 'completed_exercises'
-completed_exercises = db.Table(completed_exercises_NAMETAG,
-    db.Column('user_id', db.Integer, db.ForeignKey('users.id')),
-    db.Column('exercise_id', db.Integer, db.ForeignKey('exercises.id'))
-)
+class UserCompletedExercise(db.Model):
+    __tablename__ = 'user_completed_exercises'
+
+    user_id = db.Column(db.Integer, ForeignKey('users.id'), primary_key=True)
+    exercise_id = db.Column(db.Integer, ForeignKey('exercises.id'), primary_key=True)
+    times_completed = db.Column(db.Integer, nullable=False, default=0)
+
+
+# completed_exercises_NAMETAG = 'completed_exercises'
+# completed_exercises = db.Table(completed_exercises_NAMETAG,
+#     db.Column('user_id', db.Integer, db.ForeignKey('users.id')),
+#     db.Column('exercise_id', db.Integer, db.ForeignKey('exercises.id'))
+# )
--- qlerner/models/lesson.py
+++ qlerner/models/lesson.py
@@ -8,16 +8,31 @@ class Lesson(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String, nullable=False)
     tags = db.Column(db.String)  # comma-separated list of tags
+    exercise_ids = db.Column(db.String) # comma-separated list of exercises
+
+class UserCompletedLesson(db.Model):
+    __tablename__ = 'user_completed_lessons'    
+
+    user_id = db.Column(db.Integer, ForeignKey('users.id'), primary_key=True)
+    lesson_id = db.Column(db.Integer, ForeignKey('lessons.id'), primary_key=True)
+    # completed_at = db.Column(db.DateTime)  # Unix timestamp
+    times_completed = db.Column(db.Integer, nullable=False, default=0)
+
+class ExercisesForLesson(db.Model):
+    __tablename__ = 'exercises_for_lesson'
+
+    exercise_id = db.Column(db.Integer, ForeignKey('exercises.id'), primary_key=True)
+    lesson_id = db.Column(db.Integer, ForeignKey('lessons.id'), primary_key=True)
 
 user_completed_lessons_NAMETAG = 'user_completed_lessons'
-user_completed_lessons = db.Table(user_completed_lessons_NAMETAG,
-    db.Column('user_id', db.Integer, ForeignKey('user.id'), primary_key=True),
-    db.Column('lessons_id', db.Integer, ForeignKey('lessons.id'), primary_key=True),
-    db.Column('completed_at', db.DateTime)
-)
+# user_completed_lessons = db.Table(user_completed_lessons_NAMETAG,
+#     db.Column('user_id', db.Integer, ForeignKey('user.id'), primary_key=True),
+#     db.Column('lessons_id', db.Integer, ForeignKey('lessons.id'), primary_key=True),
+#     db.Column('completed_at', db.DateTime)
+# )
 
 exercises_for_lesson_NAMETAG = 'exercises_for_lesson'
-exercises_for_lesson = db.Table(exercises_for_lesson_NAMETAG,
-    db.Column('exercises_id', db.Integer, ForeignKey('exercises.id'), primary_key=True),
-    db.Column('lessons_id', db.Integer, ForeignKey('lessons.id'), primary_key=True)
-)
+# exercises_for_lesson = db.Table(exercises_for_lesson_NAMETAG,
+#     db.Column('exercises_id', db.Integer, ForeignKey('exercises.id'), primary_key=True),
+#     db.Column('lessons_id', db.Integer, ForeignKey('lessons.id'), primary_key=True)
+# )
