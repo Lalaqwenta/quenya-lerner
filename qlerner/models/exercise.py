e4251cdf7c92499d54bed27b231391ee95322e18
Lalaqwenta <plmisha97@gmail.com>
2023-03-02 11:51:26 +0300

    Added dictionary and chose one exercises

--- qlerner/models/exercise.py
+++ qlerner/models/exercise.py
@@ -2,12 +2,15 @@ from sqlalchemy import ForeignKey
 from sqlalchemy.orm import relationship
 from qlerner.main.database import db
 
+completed_exercises_NAMETAG = 'completed_exercises'
+
 class Exercise(db.Model):
     __tablename__ = 'exercises'
 
     id = db.Column(db.Integer, primary_key=True)
     question = db.Column(db.Text, nullable=False)
     answer = db.Column(db.Text, nullable=False)
+    type = db.Column(db.Text, nullable=False, default='write')
     hint = db.Column(db.String(100))
     tags = db.Column(db.String)  # comma-separated list of tags
 
@@ -17,10 +20,3 @@ class UserCompletedExercise(db.Model):
     user_id = db.Column(db.Integer, ForeignKey('users.id'), primary_key=True)
     exercise_id = db.Column(db.Integer, ForeignKey('exercises.id'), primary_key=True)
     times_completed = db.Column(db.Integer, nullable=False, default=0)
-
-
-# completed_exercises_NAMETAG = 'completed_exercises'
-# completed_exercises = db.Table(completed_exercises_NAMETAG,
-#     db.Column('user_id', db.Integer, db.ForeignKey('users.id')),
-#     db.Column('exercise_id', db.Integer, db.ForeignKey('exercises.id'))
-# )
--- qlerner/models/word.py
+++ qlerner/models/word.py
@@ -1,6 +1,8 @@
 from qlerner.main.database import db
 
 class Word(db.Model):
+    __tablename__ = 'words'
+    
     id = db.Column(db.Integer, primary_key=True)
     english_meaning = db.Column(db.String(256), nullable=False)
     english_translation = db.Column(db.String(256), nullable=False)
